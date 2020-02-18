25c83be66350ce1cb29082ef88947f2c44b8de2b
Rafael dos Santos <rafael.santos@i9tec.com.br>
2020-02-18 10:45:09 -0300

    Criado controller config

---
+++ Itau.Sustentacao/Itau.Painel/Controllers/ConfigController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Itau.Controllers
+{
+    public class ConfigController : Controller
+    {
+        // GET: Config
+        public ActionResult Index(string equipe)
+        {
+            if (!string.IsNullOrEmpty(equipe))
+            {
+                ViewBag.equipe = equipe;
+                ViewBag.conteudoJson = string.Empty;
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader(Server.MapPath($"~/Dados/{equipe}/slides-{equipe}.json"), Encoding.GetEncoding("ISO-8859-1")))
+                    {
+                        string conteudo = sr.ReadToEnd();
+                        ViewBag.conteudoJson = conteudo;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+
+            }
+            return View();
+        }
+    }
+}
